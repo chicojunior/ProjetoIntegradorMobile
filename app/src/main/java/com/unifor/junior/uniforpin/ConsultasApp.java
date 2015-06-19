5dcac7deef8d89c7f7b07939403cb24163daa4a3
chicojunior <chicojunior@github.com>
2015-06-19 18:30:37 -0300

    Classe consulta.

---
+++ app/src/main/java/com/unifor/junior/uniforpin/ConsultasApp.java
@@ -0,0 +1,10 @@
+package com.unifor.junior.uniforpin;
+
+import android.app.Application;
+
+/**
+ * Created by Junior on 18/06/2015.
+ */
+public class ConsultasApp extends Application{
+    public static final Medico[] medicos ={new Medico("João de Deus", "Cardiologista", "1510")};
+}
